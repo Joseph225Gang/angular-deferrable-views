fab7163f429e47a052909bfbaa21be83d1dc79f6
MSI\lai22 <lai225joseph@hotmail.com>
2024-12-10 17:43:59 +0800

    'deferrable-views'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-deferrable-views';
+  count = 0;
+
+  addClick()
+  {
+    ++this.count;
+  }
 }
